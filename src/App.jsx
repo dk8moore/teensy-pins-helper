8d9f1fccccacfc125a4d4404f8227bb38e53ae61
Denis Ronchese <denis.ronchese@gmail.com>
2025-02-05 13:28:33 +0100

    First setup of React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TeensyConfigurator from './components/TeensyConfigurator';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <TeensyConfigurator />
+    </div>
+  );
+}
+
+export default App;
