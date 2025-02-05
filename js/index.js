4b0f802cfd7852810b93d4edd846ab479ac3754f
Denis Ronchese <denis.ronchese@gmail.com>
2025-02-05 14:48:09 +0100

    Remove deprecated vanilla design

--- js/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-import { TeensyConfigApp } from './core/app.js';
-
-// This file serves as the main entry point for the application
-// The TeensyConfigApp class is instantiated when the DOM is loaded
-document.addEventListener('DOMContentLoaded', () => {
-    new TeensyConfigApp();
-});
