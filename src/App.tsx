a56a79f61aaaaefeaa7689d50337aaddf75f9de5
Denis Ronchese <denis.ronchese@gmail.com>
2025-02-28 00:02:32 +0100

    Migrate main and app components

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TeensyConfigurator from "@/components/TeensyConfigurator.tsx";
+
+function App(): React.ReactElement {
+  return (
+    <div className="min-h-screen bg-background">
+      <TeensyConfigurator />
+    </div>
+  );
+}
+
+export default App;
